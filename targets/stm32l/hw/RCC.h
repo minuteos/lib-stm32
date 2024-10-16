deb6f40a92fc3507407e8ff2948462b3b2ffd0ac
Štefan Šimek <simek@triaxis.sk>
2024-10-16 17:50:17 +0200

    stm32l: hw - add basic support for I2C master mode

---
+++ targets/stm32l/hw/RCC.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2024 triaxis s.r.o.
+ * Licensed under the MIT license. See LICENSE.txt file in the repository root
+ * for full license information.
+ *
+ * stm32l/hw/RCC.h
+ */
+
+#pragma once
+
+#include <base/base.h>
+
+#undef RCC
+#define RCC CM_PERIPHERAL(_RCC, RCC_BASE)
+
+struct _RCC : RCC_TypeDef
+{
+    void EnableI2C(unsigned index)
+    {
+        ASSERT(index < 3);
+        APB1ENR1 |= RCC_APB1ENR1_I2C1EN << index;
+    }
+};
